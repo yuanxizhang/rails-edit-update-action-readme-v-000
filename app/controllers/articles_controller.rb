be3d65d86d9cb530830d0568bed8aba3fc1259be
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-08 21:55:29 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,8 @@ class ArticlesController < ApplicationController
   end 
   
   def update
-  @article = Article.find(params[:id])
-  @article.update(title: params[:title], description: params[:description])
-  redirect_to article_path(@article)
-end
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
+    redirect_to article_path(@article)
+  end
 end
